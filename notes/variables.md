915e8eb80c57e4c963781d3c528e2a0f949743c7
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-04-30 15:45:16 +0200

    Add tensor flow basic example
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

---
+++ notes/variables.md
@@ -0,0 +1,7 @@
+### Independant variables
+Are vars that are do not depend on other variables. As an example the birth
+date of a person is an indepentent variable. Two people with the same birth date
+do not have to have the same height, or be from the same part of the country.
+Here height or part of the country are other variables but they are not related
+dependent on the birth date.
+They are indepentent but 
---
+++ tensor-flow/src/first.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+
+dense = Dense(units=1, input_shape=[1])
+model = Sequential([dense])
+# Stochastic Gradient Decent
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+# y = 2x - 1
+xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
+
+model.fit(xs, ys, epochs=500)
+
+# ask the model to predict what y will be when x is 10.0
+x = 10.0
+y = model.predict([x])
+print(f'predicted y to be {y}, when x is {x}')
+weights = dense.get_weights()
+print(f'y = {weights[0][0]}x {weights[1]}')
+# y = 2*10 -1
+# y = 19
