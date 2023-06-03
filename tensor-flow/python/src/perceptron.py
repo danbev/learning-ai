d3359b11ee621b7a6a9e0b19e361fb39a3146dee
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-06-03 07:04:22 +0200

    Add example of a perceptron neuron computation
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

---
+++ tensor-flow/python/src/perceptron.py
@@ -0,0 +1,21 @@
+def perceptron_neuron(x, w):
+    z = 0.0
+    for i in range(len(w)):
+        z += x[i] * w[i]
+
+    return signum(z)
+
+def signum(s):
+    if s < 0: # sign/signum function
+        return -1
+    else:
+        return 1
+
+
+# Only valid input values are 1 and -1
+xs = [1, -1.0, -1.0]
+ws = [0.9, -0.6, -0.5]
+
+output = perceptron_neuron(ws, xs)
+print(output)
+
