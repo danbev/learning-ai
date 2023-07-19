f48c9ce76b89fd871ab8a8ca04f1fd85eb22961e
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-07-19 06:47:01 +0200

    Add more dataset exploration code
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- hugging-face/python/src/ds.py
+++ hugging-face/python/src/ds.py
@@ -1,5 +1,6 @@
 from datasets import list_datasets
 from datasets import load_dataset
+#from huggingface_hub import list_datasets
 
 ds_sets = list_datasets()
 print(f'Number of datasets: {len(ds_sets)}')
@@ -11,3 +12,11 @@ emotions = load_dataset("dair-ai/emotion")
 
 # Print emotion
 print(emotions)
+
+train_ds = emotions['train']
+print(f'Columns: {train_ds.column_names}')
+print(train_ds)
+print(train_ds[0])
+print(train_ds[1])
+print(train_ds[1]["text"])
+print(train_ds[1]["label"])
