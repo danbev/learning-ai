4ea0ff38dd6058e8bf6963bdefdd804bdabe4bcf
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-07-20 06:34:21 +0200

    Add more tokens and one-hot encoding examples
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- hugging-face/python/src/tokens.py
+++ hugging-face/python/src/tokens.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 # Tokens section
 text = "Tokenizing text is a core task of NLP."
 tokenized_text = list(text)
@@ -10,3 +12,22 @@ print(f'token2idx: {token2idx}')
 
 input_ids = [token2idx[token] for token in tokenized_text]
 print(f'Tokens: {input_ids}')
+
+print(f"Index of \'T\': {token2idx['T']}")
+
+
+categorical_df = pd.DataFrame(
+    {"Name": ["Bumblebee", "Optimus Prime", "Megatron"], "Label ID": [0,1,2]})
+print(categorical_df)
+
+import torch
+import torch.nn.functional as F
+
+input_ids = torch.tensor(input_ids)
+one_hot_encodings = F.one_hot(input_ids, num_classes=len(token2idx))
+print(one_hot_encodings)
+print(one_hot_encodings.shape)
+
+print(f"Token: {tokenized_text[0]}")
+print(f"Tensor index: {input_ids[0]}")
+print(f"One-hot: {one_hot_encodings[0]}")
