48542b473db9034140373d98c120529190b365ff
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-07-20 06:53:38 +0200

    Add more tokenization examples
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- hugging-face/python/src/tokens.py
+++ hugging-face/python/src/tokens.py
@@ -31,3 +31,40 @@ print(one_hot_encodings.shape)
 print(f"Token: {tokenized_text[0]}")
 print(f"Tensor index: {input_ids[0]}")
 print(f"One-hot: {one_hot_encodings[0]}")
+
+# Word tokens examples
+tokenized_text = text.split()
+print(tokenized_text)
+# Notice that the last element of the list contains a period. 
+
+# Wordpiece tokens examples which is used by BERT and DistilBERT tokenizers.
+from transformers import AutoTokenizer
+# From chatgpt 4:
+"""
+In the context of Hugging Face models, a checkpoint typically refers to the
+saved state of a trained model. It includes both the model architecture and the
+learned parameters (or weights).
+
+While training large models, it is common practice to periodically save
+checkpoints. If the training process gets interrupted for any reason, you can
+resume from the last saved checkpoint, rather than starting from scratch. This
+can save a lot of computational resources and time.
+
+In Hugging Face's Model Hub, a "checkpoint" often refers to a specific model
+that has been pre-trained and shared publicly. For example, "bert-base-uncased"
+and "gpt-2" are names of checkpoints. You can load these checkpoints directly
+using the from_pretrained method, which allows you to leverage the power of
+pre-trained models for your specific tasks with additional fine-tuning.
+"""
+model_checkpoint = "distilbert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+encoded_text = tokenizer(text)
+print(encoded_text)
+tokens = tokenizer.convert_ids_to_tokens(encoded_text.input_ids)
+print(tokens)
+print(tokenizer.convert_tokens_to_string(tokens))
+print(f'vocab size: {tokenizer.vocab_size}')
+# print max length of tokenizer
+print(f'max length: {tokenizer.model_max_length}')
+# print input names of tokenizer
+print(f'input names: {tokenizer.model_input_names}')
