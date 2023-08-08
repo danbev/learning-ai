91bfc080e86ed5f8f0785c17d5116cb21911e0d5
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-08-08 18:13:45 +0200

    Update label format for nodes in autograd.py
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- fundamentals/python/src/autograd.py
+++ fundamentals/python/src/autograd.py
@@ -161,7 +161,7 @@ def draw_dot(root):
     nodes, edges = trace(root)
     for n in nodes:
         uid = str(id(n))
-        dot.node(name = uid, label="{ data %.4f }" % (n.data,), shape='record')
+        dot.node(name = uid, label=f'data: {n.data:.4f}', shape='record')
         if n._op:
             dot.node(name = uid + n._op, label=n._op)
             dot.edge(uid + n._op, uid)
