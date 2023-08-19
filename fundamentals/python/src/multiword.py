b6ad623df9f28ac95042d61fb25d8df2219ce2ee
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-08-19 13:50:14 +0200

    Add note about the matrix multiplication of one_hot
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- fundamentals/python/src/multiword.py
+++ fundamentals/python/src/multiword.py
@@ -15,7 +15,7 @@ itos = {i: ch for ch, i in stoi.items()}
 # Build the dataset
 # Block size is the context length, as in how many characters to use to predict
 # the next character.
-block_size = 103
+block_size = 3
 print(f'block_size: {block_size}')
 X = []
 Y = []
@@ -38,5 +38,48 @@ for word in words[:5]:
 
 X = torch.tensor(X)
 Y = torch.tensor(Y)
+# The inputs 32x3 are the 32 words and the 3 characters that we are using to
+# predict the next character.
+print(f'{X.shape=}, {X.dtype=} (Inputs)')
+print(f'{Y.shape=}, {Y.dtype=} (Labels)')
 print(X)
 print(Y)
+# We are going to take the X's and predicts the Y values for those inputs.
+C = torch.randn(27, 2) # 27 characters (rows), 2 dimensions embedding (columns)
+print(f'{C=}')
+
+# Manual embedding of the integer 5:
+# We have 27 possible characters.
+one_hot = F.one_hot(torch.tensor(5), num_classes=27).float()
+print(f'{one_hot=}')
+one_hot = one_hot @ C
+# Notice that this matrix multiplication is like masking out the fifth row:
+# [0.0] [ 0.1961, -0.7622]
+# [0.0] [-1.1182,  0.6680]
+# [0.0] [ 0.3447,  0.3149]
+# [0.0] [-0.5860,  0.7838]
+# [0.0] [ 1.0652,  1.4711]
+# [1.0] [-0.8589, -0.8324]
+# [0.0] [ 0.6763,  0.6351]
+# [0.0] [-0.8141, -0.2554]
+# [0.0] [-0.1614, -0.5273]
+# [0.0] [-0.4246,  0.2074] = [-0.8589, -0.8324]
+# [0.0] [-0.7300, -0.6255]
+# [0.0] [-0.2981,  0.3134]
+# [0.0] [-0.2918, -0.4503]
+# [0.0] [-0.8250,  0.8351]
+# [0.0] [ 0.8814, -0.2109]
+# [0.0] [-0.2941,  0.4049]
+# [0.0] [ 0.6671,  0.4552]
+# [0.0] [-0.0369,  1.2453]
+# [0.0] [-2.3761, -1.0131]
+# [0.0] [ 1.2863,  0.5841]
+# [0.0] [-0.7500, -0.2501]
+# [0.0] [-0.0798, -0.2308]
+# [0.0] [-0.3383, -0.8850]
+# [0.0] [ 0.3755, -0.7705]
+# [0.0] [-1.1456,  0.3123]
+# [0.0] [-0.7670, -1.9977]
+# [0.0] [ 0.0691,  1.4713]
+print(C[5])
+print(f'{one_hot=}.item()')
