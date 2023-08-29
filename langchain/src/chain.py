d5baccd7ce40736d931dc9b4dc3ae1c7526625a5
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-08-29 15:35:01 +0200

    Add SequentialChain example
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- langchain/src/chain.py
+++ langchain/src/chain.py
@@ -49,3 +49,63 @@ overall_simple_chain = SimpleSequentialChain(chains=[chain_one, chain_two],
                                             )
 result = overall_simple_chain.run(product)
 print(result)
+
+
+from langchain.chains import SequentialChain
+
+llm = ChatOpenAI(temperature=0.9, model=llm_model)
+
+# prompt template 1: translate to english
+first_prompt = ChatPromptTemplate.from_template(
+    "Translate the following review to english:"
+    "\n\n{Review}"
+)
+# chain 1: input= Review and output= English_Review
+chain_one = LLMChain(llm=llm, prompt=first_prompt, 
+                     output_key="English_Review"
+                    )
+
+second_prompt = ChatPromptTemplate.from_template(
+    "Can you summarize the following review in 1 sentence:"
+    "\n\n{English_Review}"
+)
+# chain 2: input= English_Review and output= summary
+chain_two = LLMChain(llm=llm, prompt=second_prompt,
+                     output_key="summary"
+                    )
+
+
+# prompt template 3: identify language
+third_prompt = ChatPromptTemplate.from_template(
+    "What language is the following review:\n\n{Review}"
+)
+# chain 3: input= Review and output= language
+chain_three = LLMChain(llm=llm, prompt=third_prompt,
+                       output_key="language"
+                      )
+
+
+# prompt template 4: follow up message
+fourth_prompt = ChatPromptTemplate.from_template(
+    "Write a follow up response to the following "
+    "summary in the specified language:"
+    "\n\nSummary: {summary}\n\nLanguage: {language}"
+)
+# chain 4: input= summary, language and output= followup_message
+chain_four = LLMChain(llm=llm, prompt=fourth_prompt,
+                      output_key="followup_message"
+                     )
+
+
+# overall_chain: input= Review
+# and output= English_Review,summary, followup_message
+overall_chain = SequentialChain(
+    chains=[chain_one, chain_two, chain_three, chain_four],
+    input_variables=["Review"],
+    output_variables=["English_Review", "summary","followup_message"],
+    verbose=True
+)
+
+review = df.Review[5]
+result = overall_chain(review)
+print(result)
