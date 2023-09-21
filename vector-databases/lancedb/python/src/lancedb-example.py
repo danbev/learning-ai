ee878c02c38404492d35692af2456c070a58ef08
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-09-21 08:59:18 +0200

    src: update lancedb python example to use schema
    This commit updates the lancedb python example to use a schema when
    creating the table. This is to be able to have a Rust example that does
    the same thing (which I believe is not possible without a schema).
    
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- vector-databases/lancedb/python/src/lancedb-example.py
+++ vector-databases/lancedb/python/src/lancedb-example.py
@@ -1,4 +1,5 @@
 import lancedb
+import pyarrow as pa
 import os
 
 uri = "./data/sample-lancedb"
@@ -7,12 +8,17 @@ db = lancedb.connect(uri)
 # check if the database exists and only proceed if it does not
 if db.table_names() == []:
     print("Creating table")
-    table = db.create_table(table_name,
-                         data=[{"vector": [3.1, 4.1], "item": "foo", "price": 10.0},
-                               {"vector": [5.9, 26.5], "item": "bar", "price": 20.0}])
+    schema = pa.schema([pa.field("vector", pa.list_(pa.float32(), list_size=2)),
+                        pa.field("item", pa.string()),
+                        pa.field("price", pa.float32())])
+    #print(f'{schema=}')
+    table = db.create_table(table_name, schema=schema)
+    table.add(data=[{"vector": [3.1, 4.1], "item": "foo", "price": 10.0},
+                    {"vector": [5.9, 26.5], "item": "bar", "price": 20.0}])
 else:
     print("Opening table")
     table = db.open_table(table_name)
 
+print("Table:", table.name)
 result = table.search([100, 100]).limit(2).to_df()
 print(result)
