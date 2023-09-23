d6a98d73427ddfe9d7513b73ad07e09e376e181d
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-09-23 17:12:16 +0200

    src: add output layer to mlp
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- fundamentals/rust/zero-to-hero/src/mlp.rs
+++ fundamentals/rust/zero-to-hero/src/mlp.rs
@@ -32,7 +32,7 @@ impl<const I: usize, const N: usize, const L: usize> Mlp<I, N, L> {
 
 #[allow(dead_code)]
 impl<const I: usize, const N: usize, const L: usize> FnOnce<(Vec<Rc<Value>>,)> for Mlp<I, N, L> {
-    type Output = Vec<Rc<Value>>;
+    type Output = Rc<Value>;
 
     extern "rust-call" fn call_once(self, xs: (Vec<Rc<Value>>,)) -> Self::Output {
         // The output of one layer is the input to the next layer.
@@ -40,7 +40,8 @@ impl<const I: usize, const N: usize, const L: usize> FnOnce<(Vec<Rc<Value>>,)> f
         for layer in self.layers.into_iter() {
             layer_output = layer(layer_output);
         }
-        layer_output
+        let last = Layer::<I, 1>::new();
+        last(layer_output)[0].clone()
     }
 }
 
@@ -60,7 +61,7 @@ mod tests {
     fn test_mlp_call() {
         let layer = Mlp::<1, 3, 2>::new();
         let inputs = vec![Rc::new(Value::new_with_label(1.0, "x0"))];
-        let outputs = layer(inputs);
-        assert_eq!(outputs.len(), 3);
+        let output = layer(inputs);
+        println!("output: {}", output.dot());
     }
 }
