9196ff9179aced70a1d67d76b8e1195a5864189d
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-09-28 06:10:44 +0200

    src: use AgentType in agent-serper.py
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- langchain/src/agent-serper.py
+++ langchain/src/agent-serper.py
@@ -3,6 +3,7 @@ import os
 from langchain.llms import OpenAI
 from langchain.agents import load_tools
 from langchain.agents import initialize_agent
+from langchain.agents import AgentType
 
 from dotenv import load_dotenv 
 load_dotenv()
@@ -12,7 +13,14 @@ os.environ["SERPER_API_KEY"] = os.getenv("SERPER_API_KEY")
 
 llm = OpenAI(model_name="text-davinci-003" ,temperature=0)
 tools = load_tools(["google-serper", "llm-math"], llm=llm)
-agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
+
+# In this case the AgentType is CHAT_ZERO_SHOT_REACT_DESCRIPTION which is
+# a zero-shot meaning that the complete context is provided by the agent and
+# there is no memory of previous interactions. The REACT part is the ReAct,
+# reason and act, part of the agent. The description indicates that the agent
+# will trigger tools based on the description of the tool.
+agent = initialize_agent(tools, llm, agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+    
 
 result = agent.run("Who is Austin Powers? What is his current age raised to the 0.23 power? Return his age in the result")
 print(f'{result=}')
