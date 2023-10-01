44693572d169bd7807866627eb0ba02dda9ce476
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-10-01 09:02:03 +0200

    src: make c-example a c example and not C++
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- fundamentals/ggml/c-example/src/main.c
+++ fundamentals/ggml/c-example/src/main.c
@@ -1,16 +1,17 @@
-#include <iostream>
+#include <stdio.h>
 
 #include "ggml/ggml.h"
 #include "ggml/ggml-alloc.h"
 
-int main()
-{
-  std::cout << "GGML Example" << std::endl;
+int main(int argc, char **argv) {
+  printf("GGML Example\n");
+
   struct ggml_init_params params = {
     .mem_size   = 16*1024*1024,
     .mem_buffer = NULL,
   };
   struct ggml_context *ctx = ggml_init(params);
+  printf("ctx mem_buffer: %ld\n", ggml_get_mem_size(ctx));
   ggml_free(ctx);
   return 0;
 }
