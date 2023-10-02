afb0cdc782f3fa45f962ae9a362d4322460f5ba0
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-10-02 11:40:42 +0200

    src: add request example to agent-custom-tool.py
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- langchain/src/agent-custom-tool.py
+++ langchain/src/agent-custom-tool.py
@@ -6,22 +6,47 @@ from langchain.agents import Tool
 from langchain.tools import BaseTool
 from langchain.agents import load_tools, Tool, initialize_agent, AgentType
 
+from langchain.requests import RequestsWrapper
+import requests
+
 from dotenv import load_dotenv 
 load_dotenv()
+
+def vex(input):
+    token_endpoint = 'http://localhost:8090/realms/chicken/protocol/openid-connect/token'
+    client_id = 'walker'
+    client_secret = os.getenv("CLIENT_SECRET")
+    access_token = []
+
+    response = requests.post(
+        token_endpoint,
+        data={
+            'grant_type': 'client_credentials',
+            'client_id': client_id,
+            'client_secret': client_secret,
+        }
+    )
+    if response.status_code == 200:
+        token_data = response.json()
+        access_token = token_data['access_token']
+    else:
+        print(f"Failed to obtain access token. Status code: {response.status_code}, Response: {response.text}")
+        exit()
+
+    headers = {"Authorization": f"Bearer {access_token}"}
+    requests_wrapper = RequestsWrapper(headers=headers)
+    return requests_wrapper.get(f'http://localhost:8081/api/v1/vex?advisory=${input}')
+
+vex_tool = Tool(
+    name='VEX',
+    func= vex,
+    description="Useful when you need to get information related to a VEX using its advisory ID. An example of an advisory ID is RHSA-2023:1441"
+)
 
 #llm = OpenAI(model_name="gpt-3.5-turbo" ,temperature=0)
 llm = OpenAI(model_name="text-davinci-003" ,temperature=0)
 
 tools = load_tools(["google-serper", "llm-math"], llm=llm)
-
-def bajja(input=""):
-    return 'Bajja is a silly thing that is sometimes put in debug messages when I dont know what to write.'
-
-bajja_tool = Tool(
-    name='Bajja',
-    func= bajja,
-    description="Useful for when you need to answer questions about Bajja. input should be bajja "
-)
 
 def random_num(input=""):
     print(f'In random_num function. input: {input}')
@@ -32,8 +57,8 @@ random_tool = Tool(
     func= random_num,
     description="Useful for when you need to get a random number. input should be 'random'"
 )
-tools.append(bajja_tool)
 tools.append(random_tool)
+tools.append(vex_tool)
 
 memory = ConversationBufferWindowMemory(
     memory_key='chat_history',
@@ -51,54 +76,17 @@ agent_executor = initialize_agent(
     memory=memory
 )
 
-result = agent_executor("What time is it in Brisbase?")
+result = agent_executor("What time is it in Brisbane?")
 print(result['output'])
 
 result = agent_executor("Can you show me a random number?")
 print(result['output'])
 
-result = agent_executor("What is the meaning of bajja?")
+result = agent_executor("Can you tell me what the VEX RHSA-2023:1441 is about?")
+print(result['output'])
+
+result = agent_executor("Which CVEs are related to the VEX RHSA-2023:1441?")
 print(result['output'])
 
 #print(agent_executor.agent.llm_chain.prompt.messages[0].prompt.template)
-updated_prompt = """
-Answer the following questions as best you can. You have access to the following tools:
-
-google_serper: A low-cost Google Search API.Useful for when you need to answer questions about current events.Input should be a search query.
-Calculator: Useful for when you need to answer questions about math.
-Meaning of Life: Useful for when you need to answer questions about the meaning of life. input should be MOL
-Random number: Useful for when you need to get a random number. input should be 'random'
-
-The way you use the tools is by specifying a json blob.
-Specifically, this json should have a `action` key (with the name of the tool to use) and a `action_input` key (with the input to the tool going here). You don't know anything about random numbers or the meaning of life.
-
-The only values that should be in the "action" field are: google_serper, Calculator, Meaning of Life, Random number
-
-The $JSON_BLOB should only contain a SINGLE action, do NOT return a list of multiple actions. Here is an example of a valid $JSON_BLOB:
-
-```
-{{
-  "action": $TOOL_NAME,
-  "action_input": $INPUT
-}}
-```
-
-ALWAYS use the following format:
-
-Question: the input question you must answer
-Thought: you should always think about what to do
-Action:
-```
-$JSON_BLOB
-```
-Observation: the result of the action
-... (this Thought/Action/Observation can repeat N times)
-Thought: I now know the final answer
-Final Answer: the final answer to the original input question
-
-Begin! Reminder to always use the exact characters `Final Answer` when responding.
-"""
 #agent_executor.agent.llm_chain.prompt.messages[0].prompt.template = updated_prompt
-
-#result = agent_executor("What is the meaning of life?")
-#print(result['output'])
