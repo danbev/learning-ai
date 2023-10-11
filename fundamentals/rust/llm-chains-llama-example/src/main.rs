13b667a9b96220c94e7f96d44c2ae8c141571bfe
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-10-11 16:13:20 +0200

    docs: add note about PresencePenalty
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- fundamentals/rust/llm-chains-llama-example/src/main.rs
+++ fundamentals/rust/llm-chains-llama-example/src/main.rs
@@ -1,33 +1,32 @@
 use llm_chain::options::*;
-use llm_chain::prompt::Conversation;
-use llm_chain::{
-    chains::conversation::Chain, executor, options, output::StreamExt, parameters, prompt,
-    step::Step,
-};
+use llm_chain::{executor, options, output::StreamExt, parameters, prompt, step::Step};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts = options!(
         Model: ModelRef::from_path("./models/llama-2-7b-chat.ggmlv3.q4_0.bin"),
         ModelType: "llama",
-        MaxContextSize: 2000_usize,
-        MaxTokens: 50_usize,
+        MaxContextSize: 3000_usize,
+        MaxTokens: 2000_usize,
         Temperature: 0.1,
-        RepeatPenalty: 1.1,
-        RepeatPenaltyLastN: 64_usize,
-        FrequencyPenalty: 0.0,
-        PresencePenalty: 0.0,
-        Mirostat: 0_i32,
-        MirostatTau: 5.0,
+        TopP: 0.1,
+        RepeatPenalty: 8.4,
+        RepeatPenaltyLastN: 1000_usize,
+        FrequencyPenalty: 1.0,
+        PresencePenalty: 1.0,
+        Mirostat: 1_i32,
+        MirostatTau: 1.0,
         MirostatEta: 0.1,
         PenalizeNl: true,
-        NThreads: 4_usize
-        //StopSequence: vec!["\n".to_string()]
+        NThreads: 4_usize,
+        Stream: false
+        //StopSequence: vec!["\n".to_string(), "\n\n".to_string(), "</s>".to_string(), "LP".to_string()]
     );
     let exec = executor!(llama, opts.clone())?;
     let step = Step::for_prompt_with_streaming(prompt!(
-        "You are a helpful assistant that answers quesetions. The question is:\n--\n{{query}}\n--\n"));
-    let query = "What is the meaning of life?";
+        "You are a helpful assistant that answers questions.The question is:\n--\n{{query}}\n--\n"
+    ));
+    let query = "What is LoRa?";
     println!("Query: {}", query);
     //let res = Step::for_prompt_with_streaming(chat_prompt)
     let res = step.run(&parameters!().with("query", query), &exec).await?;
