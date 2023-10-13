972593c79d512a96f1878ad54788ab9f1dfe22a7
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-10-13 09:58:46 +0200

    src: add loading of model from file
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- fundamentals/llama.cpp/src/simple-prompt.cpp
+++ fundamentals/llama.cpp/src/simple-prompt.cpp
@@ -6,7 +6,17 @@
 int main(int argc, char** argv) {
   std::cout << "llama.cpp example" << std::endl;
   gpt_params params;
+  params.model = "models/llama-2-13b-chat.Q4_0.gguf";
   std::cout << "params.n_threads: " << params.n_threads << std::endl;
+
+  llama_backend_init(params.numa);
+  llama_model_params model_params = llama_model_default_params();
+
+  llama_model* model = llama_load_model_from_file(params.model.c_str(), model_params);
+  if (model == NULL) {
+      fprintf(stderr , "%s: error: failed to to load model %s\n" , __func__, params.model.c_str());
+      return 1;
+  }
 
   return 0;
 }
