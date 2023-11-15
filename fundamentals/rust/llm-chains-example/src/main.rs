fa9ad3f61063e9e8d6ee57430178c5bc170a5b00
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-11-15 05:53:36 +0100

    src: add print of query in OpenAI ChatGPT example
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- fundamentals/rust/llm-chains-example/src/main.rs
+++ fundamentals/rust/llm-chains-example/src/main.rs
@@ -2,11 +2,12 @@ use llm_chain::{executor, parameters, prompt};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("OpenAI ChatGPT example");
     // Create a new ChatGPT executor
     let exec = executor!()?;
-    let res = prompt!("", "Explain positional encoding",)
-        .run(&parameters!(), &exec)
-        .await?;
+    let query = "Explain positional encoding";
+    println!("Query: {query}");
+    let res = prompt!("", query,).run(&parameters!(), &exec).await?;
     println!("{res}");
     Ok(())
 }
