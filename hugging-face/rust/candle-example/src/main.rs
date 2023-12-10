d5ae745a511e8a0b6ceaa693f6cb0776cdeb13e3
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-12-10 13:43:24 +0100

    src: add comment about setting seed for CPU backend
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- hugging-face/rust/candle-example/src/main.rs
+++ hugging-face/rust/candle-example/src/main.rs
@@ -1,7 +1,12 @@
+use candle_core::backend::BackendDevice;
+use candle_core::cpu_backend::CpuDevice;
 use candle_core::{Device, Tensor};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let device = Device::Cpu;
+
+    // Is is currently not possible to set the seed for the CPU backend.
+    //CpuDevice.set_seed(42).expect("Failed to set seed");
 
     let a = Tensor::randn(0f32, 1., (2, 3), &device)?;
     let b = Tensor::randn(0f32, 1., (3, 4), &device)?;
