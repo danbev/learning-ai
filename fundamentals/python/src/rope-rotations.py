e54a6e38b76c5038e7158c4a308b08d9a19e31bf
Daniel Bevenius <daniel.bevenius@gmail.com>
2024-04-17 07:46:22 +0200

    python: update the rope-rotations.py example
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- fundamentals/python/src/rope-rotations.py
+++ fundamentals/python/src/rope-rotations.py
@@ -1,8 +1,12 @@
 d = 4096  # dimensionality of the embedding space
-
 nr = 10
+base = 10000
 
+print(f'd={d}, base={base}, nr={nr}')
 for i in range(0, nr):
-    theta = 10000**(-2*(i-1)/d)
+    theta = base**(-2*(i-1)/d)
     print(theta)
-
+print('---')
+for i in range(0, 10):
+    exponential = (-2*(i-1)/d)
+    print(exponential)
