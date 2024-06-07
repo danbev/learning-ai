4f5c2a90c52822abf5c42dbf057f2279f4095863
Daniel Bevenius <daniel.bevenius@gmail.com>
2024-06-07 05:54:58 +0200

    src: add ssse3 example issue example
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

---
+++ fundamentals/simd/src/ssse3.c
@@ -0,0 +1,18 @@
+// The intention of this test is to check the availability of the SSSE3
+// macro is being set correctly. This is sensitive to the order of options
+// specified to the compiler. For example, if -mssse3 is specified and later
+// -mavx is specified, the SSSE3 macro will be defined which can be somewhat
+// surprising. This is not really an issue with this simple test and one
+// Makefile but in a larger project using CMake and including multiple
+// directories all with their own CMakeLists.txt files, it can be difficult
+// to ensure that the correct flags are being passed to the compiler. Whan even
+// more concerning is that you probably won't notice this issue until you
+// run the code or inspect it.
+#ifdef __SSSE3__
+#error "SSSE3 is still defined"
+#endif
+
+int main() {
+  return 0;
+}
+
