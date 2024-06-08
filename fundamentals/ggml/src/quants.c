f211be4c8259b1e6989de02d90c1b5fe27356d73
Daniel Bevenius <daniel.bevenius@gmail.com>
2024-06-08 04:47:45 +0200

    ggml: add macro guards debug statements
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- fundamentals/ggml/src/quants.c
+++ fundamentals/ggml/src/quants.c
@@ -3,6 +3,14 @@
 #include "ggml.h"
 #include "ggml-quants.h"
 
+#ifdef GGML_COMMON_DECL_C
+#warning "[danbev] GGML_COMMON_DECL_C is defined"
+#endif
+
+#ifdef GGML_COMMON_DECL_CUDA
+#warning "[danbev] GGML_COMMON_DECL_CUDA is defined"
+#endif
+
 int main(int argc, char **argv) {
   printf("GGML Quantization examples\n");
 
@@ -11,6 +19,8 @@ int main(int argc, char **argv) {
     .mem_buffer = NULL,
   };
   struct ggml_context* ctx = ggml_init(params);
+
+
 
   ggml_free(ctx);
   return 0;
