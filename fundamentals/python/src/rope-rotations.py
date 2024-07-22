0b7abc134964464c9f352a8bdab266fec15b5a6a
Daniel Bevenius <daniel.bevenius@gmail.com>
2024-07-22 15:12:52 +0200

    rope: update rope rotations example
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- fundamentals/python/src/rope-rotations.py
+++ fundamentals/python/src/rope-rotations.py
@@ -1,12 +1,38 @@
-d = 4096  # dimensionality of the embedding space
-nr = 10
+d = 512  # dimensionality of rope embedding space (1024/2)
+base = 10000
+print(f'd={d}, base={base}')
+
+print('--- Dimensions 0-10 ---')
+for i in range(0, 10):
+    theta = base**(-2*(i-1)/d)
+    print(f'theta_{i}: {theta:f}')
+
+print('')
+
+print('--- Dimensions 502-512 ---')
+for i in range(502, d):
+    theta = base**(-2*(i-1)/d)
+    print(f'theta_{i}: {theta:.16f}')
+
+print('')
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+d = 512  # dimensionality of rope embedding space
 base = 10000
 
-print(f'd={d}, base={base}, nr={nr}')
-for i in range(0, nr):
-    theta = base**(-2*(i-1)/d)
-    print(theta)
-print('---')
-for i in range(0, 10):
-    exponential = (-2*(i-1)/d)
-    print(exponential)
+# Calculate theta values for all dimensions
+theta_values = [base**(-2 * i / d) for i in range(d)]
+
+# Plot theta values against dimension indices using a normal scale
+plt.figure(figsize=(10, 6))
+plt.plot(range(d), theta_values, label=r'$\theta_i$ values')
+plt.xlabel('Dimension Index (i)')
+plt.ylabel(r'$\theta_i$ Value')
+plt.title(r'$\theta_i$ Values Across Dimensions')
+plt.grid(True, which="both", ls="--")
+plt.legend()
+plt.show()
+
+
