4b7420a9312593a26bb0dd7027f14aa0c9104dd2
Daniel Bevenius <daniel.bevenius@gmail.com>
2024-09-06 14:26:59 +0200

    ggml: add scale example

--- fundamentals/ggml/src/scale.c
+++ fundamentals/ggml/src/scale.c
@@ -5,7 +5,7 @@
 #include "ggml-backend.h"
 
 int main(int argc, char **argv) {
-  printf("GGML scale tensor examples\n");
+  printf("GGML scale examples\n");
 
   struct ggml_init_params params = {
     .mem_size   = 16*1024*1024,
@@ -13,33 +13,33 @@ int main(int argc, char **argv) {
   };
   struct ggml_context* ctx = ggml_init(params);
 
-  struct ggml_tensor* x = ggml_new_tensor_1d(ctx, GGML_TYPE_F32, 10);
-  printf("x before graph computation: \n");
-  ggml_set_name(x, "x");
-  ggml_set_f32_1d(x, 0, 1);
-  ggml_set_f32_1d(x, 1, 2);
-  ggml_set_f32_1d(x, 2, 3);
-  ggml_set_f32_1d(x, 3, 4);
-  ggml_set_f32_1d(x, 4, 5);
-  ggml_set_f32_1d(x, 5, 6);
-  ggml_set_f32_1d(x, 6, 7);
-  ggml_set_f32_1d(x, 7, 8);
-  ggml_set_f32_1d(x, 8, 9);
-  ggml_set_f32_1d(x, 9, 10);
+  struct ggml_tensor* a = ggml_new_tensor_1d(ctx, GGML_TYPE_F32, 5);
+  ggml_set_name(a, "a");
+  ggml_set_f32_1d(a, 0, 1);
+  ggml_set_f32_1d(a, 1, 2);
+  ggml_set_f32_1d(a, 2, 3);
+  ggml_set_f32_1d(a, 3, 4);
+  ggml_set_f32_1d(a, 4, 5);
 
-  for (int i = 0; i < ggml_nelements(x); i++) {
-    printf("view[%d]: %f\n", i, ggml_get_f32_1d(x, i));
+  printf("a tensor:\n");
+  for (int i = 0; i < ggml_nelements(a); i++) {
+    printf("a[%d]: %f\n", i, ggml_get_f32_1d(a, i));
   }
 
-  struct ggml_tensor* s = ggml_scale(ctx, x, 2);
+  struct ggml_tensor* result = ggml_scale(ctx, a, 2);
 
   struct ggml_cgraph* c_graph = ggml_new_graph(ctx);
-  ggml_build_forward_expand(c_graph, s);
-  ggml_graph_compute_with_ctx(ctx, c_graph, 4);
+  ggml_build_forward_expand(c_graph, result);
+  int n_threads = 4;
+  enum ggml_status st = ggml_graph_compute_with_ctx(ctx, c_graph, n_threads);
+  if (st != GGML_STATUS_SUCCESS) {
+    printf("could not compute graph\n");
+    return 1;
+  }
 
-  printf("x after graph computation: \n");
-  for (int i = 0; i < ggml_nelements(s); i++) {
-    printf("view[%d]: %f\n", i, ggml_get_f32_1d(s, i));
+  printf("result tensor:\n");
+  for (int i = 0; i < ggml_nelements(result); i++) {
+    printf("a[%d]: %f\n", i, ggml_get_f32_1d(result, i));
   }
 
   ggml_free(ctx);
