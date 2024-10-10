4cd5c60213d8062bc9a6c662d0ebb4d8701d89b6
Daniel Bevenius <daniel.bevenius@gmail.com>
2024-10-10 14:46:57 +0200

    metal: remove unused compute.cpp

--- gpu/metal/src/compute.cpp
+++
@@ -1,7 +0,0 @@
-#include <cstdint>
-
-extern "C" void simpleCompute(const float* input, float* output, uint64_t size) {
-    for (uint64_t i = 0; i < size; ++i) {
-        output[i] = input[i] * 2.0f;
-    }
-}
