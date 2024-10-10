686975735263bc880b69e0ed1b1177b935606c10
Daniel Bevenius <daniel.bevenius@gmail.com>
2024-10-10 08:21:52 +0200

    cuda: add driver version to minimal.cu

--- gpu/cuda/src/minimal.cu
+++ gpu/cuda/src/minimal.cu
@@ -9,15 +9,20 @@ int main() {
 
     int runtimeVersion = 0;
 
-    // Get CUDA runtime version
     err = cudaRuntimeGetVersion(&runtimeVersion);
     if (err != cudaSuccess) {
         printf("cudaRuntimeGetVersion failed: %s\n", cudaGetErrorString(err));
         return 1;
     }
-
     printf("CUDA Runtime version: %d.%d\n", runtimeVersion / 1000, (runtimeVersion % 100) / 10);
 
+    int driverVersion = 0;
+    err = cudaDriverGetVersion(&driverVersion);
+    if (err != cudaSuccess) {
+        printf("cudaDriverGetVersion failed: %s\n", cudaGetErrorString(err));
+        return 1;
+    }
+    printf("CUDA Driver version: %d.%d\n", driverVersion / 1000, (driverVersion % 100) / 10);
 
     err = cudaGetDeviceCount(&count);
     if (err != cudaSuccess) {
