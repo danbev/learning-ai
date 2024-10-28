8b9cdb18d7f0a6852541dd03397ed92239fb9b27
Daniel Bevenius <daniel.bevenius@gmail.com>
2024-10-28 18:09:53 +0100

    ggml: update ggml submodule to latest commit

--- fundamentals/ggml/src/ggml-quants.c
+++ fundamentals/ggml/src/ggml-quants.c
@@ -25,8 +25,7 @@ int main(int argc, char **argv) {
   float d = 0.5 / 15.0;
   block_q4_0 block_q4_0 = {
         .d = ggml_fp32_to_fp16(d),
-        .qs = { 0.2/d, 0.3/d, 0.3/d, 0.4/d, 0.5/d, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	.qs = { (uint8_t)(0.2/d), (uint8_t)(0.3/d), (uint8_t)(0.3/d), (uint8_t)(0.4/d), (uint8_t)(0.5/d), 0x00, 0x00, 0x00,
         }
     };
   printf("block_q4_0 delta: %f\n", ggml_fp16_to_fp32(block_q4_0.d));
@@ -40,13 +39,13 @@ int main(int argc, char **argv) {
   }
 
   // Get the type_trait for Q4_0
-  ggml_type_traits_t q4_0 = ggml_internal_get_type_traits(GGML_TYPE_Q4_0);
-  printf("ggml type trait name: %s\n", q4_0.type_name);
-  printf("ggml type trait block size: %ld\n", q4_0.blck_size);
-  printf("ggml type trait is_quantized: %s\n", q4_0.is_quantized ? "true" : "false");
+  const struct ggml_type_traits* q4_0 = ggml_get_type_traits(GGML_TYPE_Q4_0);
+  printf("ggml type trait name: %s\n", q4_0->type_name);
+  printf("ggml type trait block size: %lld\n", q4_0->blck_size);
+  printf("ggml type trait is_quantized: %s\n", q4_0->is_quantized ? "true" : "false");
   // The type trait contains function pointers to the quantize and dequantize
   // functions
-  q4_0.from_float(data, &block_q4_0, 32);
+  q4_0->from_float(data, &block_q4_0, 32);
   for (int i = 0; i < QK4_0/2; i++) {
     printf("block_q4_0.qs[%d]: %d\n", i, block_q4_0.qs[i]);
   }
