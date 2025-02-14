dbd0068d42547d499096a180cae0a3dda9c1bb5a
Daniel Bevenius <daniel.bevenius@gmail.com>
2025-02-14 14:11:24 +0100

    minja: add example of a statement

--- fundamentals/templates/minja/src/example.cpp
+++ fundamentals/templates/minja/src/example.cpp
@@ -6,8 +6,15 @@ using json = nlohmann::ordered_json;
 
 int main(int argc, char** argv) {
     printf("minja example!\n");
+    std::string str = R"(
+This the bos token: {{ bos_token }}
+{# The '-' below will remove whitespaces #}
+{%- if bos_token > 1000 -%}
+  Crickey, look at the size of that token!
+{%- endif -%}
+)";
 
-    auto tmpl = minja::Parser::parse("This the bos token: {{ bos_token }} ", /* options= */ {});
+    auto tmpl = minja::Parser::parse(str, /* options= */ {});
     auto context = minja::Context::make(minja::Value(json {
         {"bos_token", 12345},
     }));
