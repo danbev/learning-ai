ad035fbabacb7bb03310b8b751d3c7be84cc6ab0
Daniel Bevenius <daniel.bevenius@gmail.com>
2025-06-07 09:13:52 +0200

    simd : add example of unaligned data

--- fundamentals/simd-assembly/src/addps.s
+++ fundamentals/simd-assembly/src/addps.s
@@ -1,9 +1,16 @@
 .section .data
-    #.align 16
+    # In this case we only have the vectors to add and the result.
+    # So this will have proper alignment which is required for `movaps`.
+    # But this is fragile and if we add more data, we might get a segfault.
+    some_byte: .byte 42
+
+    .align 16
     vec1: .float 1.0, 2.0, 3.0, 4.0
-    #.align 16
+    
+    .align 16
     vec2: .float 5.0, 6.0, 7.0, 8.0
-    #.align 16
+
+    .align 16
     result: .space 16
 
 # make stack nonexecutable ("") 
@@ -19,7 +26,7 @@ main:
     movaps vec2(%rip), %xmm1
     addps %xmm1, %xmm0
     
-    # Store result
+    # Store result. movaps requires 16-byte alignment
     movaps %xmm0, result(%rip)
     
     # Return 0
