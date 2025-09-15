ed8c5f40d9f51ba2766459013162a625d07509f7
Daniel Bevenius <daniel.bevenius@gmail.com>
2025-09-15 16:28:20 +0200

    cuda: add template example

---
+++ gpu/cuda/src/template.cu
@@ -0,0 +1,74 @@
+#include <cuda_runtime.h>
+
+#include <stdio.h>
+
+template<typename T> __global__ void add_arrays(const T* a, const T* b, T* c, int n) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < n) {
+        c[idx] = a[idx] + b[idx];
+    }
+}
+
+int main() {
+    const int N = 6;
+    int size = N * sizeof(int);
+
+    int* h_a = new int[N];
+    int* h_b = new int[N];
+    int* h_c = new int[N];
+    for (int i = 0; i < N; i++) {
+        h_a[i] = i;
+        h_b[i] = i;
+    }
+
+    printf("array a:\n");
+    for (int i = 0; i < N; i++) {
+        printf("%d ", h_a[i]);
+    }
+    printf("\n");
+    printf("array b:\n");
+    for (int i = 0; i < N; i++) {
+        printf("%d ", h_b[i]);
+    }
+    printf("\n");
+
+    int* d_a;
+    int* d_b;
+    int* d_c;
+    cudaMalloc((void**)&d_a, size);
+    cudaMalloc((void**)&d_b, size);
+    cudaMalloc((void**)&d_c, size);
+
+    cudaMemcpy(d_a, h_a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, size, cudaMemcpyHostToDevice);
+
+    dim3 blocks(2);  // blocks per grid
+    dim3 threads(3); // threads per block
+    add_arrays<<<blocks, threads>>>(d_a, d_b, d_c, N);
+
+    cudaDeviceSynchronize();
+
+    cudaError_t cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "array_add kernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        return 1;
+    }
+
+    // Copy the array that the device has computed back to the host
+    cudaMemcpy(h_c, d_c, size, cudaMemcpyDeviceToHost);
+
+    printf("Added on GPU:\n");
+    for (int i = 0; i < N; i++) {
+        printf("%d ", h_c[i]);
+    }
+    printf("\n");
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+    delete[] h_a;
+    delete[] h_b;
+    delete[] h_c;
+    return 0;
+}
+
