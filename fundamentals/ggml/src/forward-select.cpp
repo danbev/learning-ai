1ba098b8323a70bc4d923ad5f4c8a844dbd1822c
Daniel Bevenius <daniel.bevenius@gmail.com>
2026-02-04 07:57:18 +0100

    ggml: add some comments to forward-select.cpp example

--- fundamentals/ggml/src/forward-select.cpp
+++ fundamentals/ggml/src/forward-select.cpp
@@ -23,10 +23,16 @@ int main(int argc, char** argv) {
     struct ggml_tensor * op_mul = ggml_mul(ctx, one, two);
     ggml_set_name(op_mul, "op_mul");
 
+    struct ggml_cgraph * c_graph = ggml_new_graph(ctx);
+
+    // Create an array with all the tensors that we want to have included in
+    // in the graph, though only one will be active.
     struct ggml_tensor * tensors[] = {op_add, op_mul};
     int n_tensors = 2;
     int active_operation_idx = 1; // change to 0 for addition operation and see different result
-    struct ggml_cgraph * c_graph = ggml_new_graph(ctx);
+
+    // Notice that in contrast to ggml_build_forward_expand this function returns
+    // the selected "active" tensor, whereas ggml_build_forward_expand does not return void.
     struct ggml_tensor * result = ggml_build_forward_select(c_graph, tensors, n_tensors, active_operation_idx);
 
     enum ggml_status st = ggml_graph_compute_with_ctx(ctx, c_graph, 1);
