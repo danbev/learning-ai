873d6a79045aa309cdd48f0599b1e046e1747432
Daniel Bevenius <daniel.bevenius@gmail.com>
2026-03-21 07:19:38 +0100

    ggml: clarify permute.cpp example

--- fundamentals/ggml/src/permute.cpp
+++ fundamentals/ggml/src/permute.cpp
@@ -9,61 +9,34 @@ int main(int argc, char **argv) {
   printf("GGML permute tensor examples\n");
 
   struct ggml_init_params params = {
-    .mem_size   = 16*1024*1024,
+    .mem_size   = (ggml_tensor_overhead() * 2) + (sizeof(float) * 3 * 2 * 4 * 1),
     .mem_buffer = NULL,
   };
   struct ggml_context* ctx = ggml_init(params);
 
-  const int nx = 2;
-  const int ny = 3;
-  struct ggml_tensor* a = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, nx, ny);
+  struct ggml_tensor* a = ggml_new_tensor_4d(ctx, GGML_TYPE_F32, 3, 2, 4, 1);
   ggml_set_name(a, "a");
-  //void* data   = (char *) a->data + i0*tensor->nb[0] + i1*tensor->nb[1] + i2*tensor->nb[2] + i3*tensor->nb[3];
-
-  ggml_set_i32_nd(a, 0, 0, 0, 0, 1);
-  ggml_set_i32_nd(a, 1, 0, 0, 0, 2);
-  ggml_set_i32_nd(a, 0, 1, 0, 0, 3);
-  ggml_set_i32_nd(a, 1, 1, 0, 0, 4);
-  ggml_set_i32_nd(a, 0, 2, 0, 0, 5);
-  ggml_set_i32_nd(a, 1, 2, 0, 0, 6);
 
   printf("a ne[0]: %ld\n", a->ne[0]);
   printf("a ne[1]: %ld\n", a->ne[1]);
   printf("a ne[2]: %ld\n", a->ne[2]);
   printf("a ne[3]: %ld\n", a->ne[3]);
 
-  printf("matrix a %ldx%ld:\n", a->ne[0], a->ne[1]);
-  for (int y = 0; y < a->ne[1]; y++) {
-      for (int x = 0; x < a->ne[0]; x++) {
-          printf("%.2f ", *(float *) ((char *) a->data + y * a->nb[1] + x * a->nb[0]));
-       }
-      printf("\n");
-  }
-  printf("\n");
-
   // A permuation is similar to a transpose, but it is a generalization of the
   // transpose operation. The transpose operation is a special case of the
-  // permute operation. The first argument is which dimension we want to move
-  // or have become the x-axis dimension and the second the which dimension
-  // index we want to move/have as the y-axis dimension.
-  //
-  // For example, if we want to turn the above 3x2 matrix into a 2x3 matrix we
-  // could permute the matrix:
-  struct ggml_tensor* p = ggml_permute(ctx, a, 1, 0, 2, 3);
-  // This is saying; use dim 1 as the new x-axis and dim 0 as the new y-axis.
-  // And keep the other dimensions as they are.
+  // permute operation.
+  // Ask: where do I want the the dim d of a to go in p.
+  struct ggml_tensor* p = ggml_permute(ctx, a, 3, 0, 1, 2);
+  // I want dim 0 of a, 3, to to in dim 3 of p:  [_, _, _, 3]
+  // I want dim 1 of a, 2, to go in dim 0 of p:  [2, _, _, 3]
+  // I want dim 2 or a, 4, to go in dim 1 of p:  [2, 4, _, 3]
+  // I want dim 3 of a, 1, to go in dim 2 of p:  [2, 4, 1, 3]
+
+  printf("Permuted:\n"); 
   printf("p ne[0]: %ld\n", p->ne[0]);
   printf("p ne[1]: %ld\n", p->ne[1]);
   printf("p ne[2]: %ld\n", p->ne[2]);
   printf("p ne[3]: %ld\n", p->ne[3]);
-
-  printf("p a %ldx%ld:\n", p->ne[0], p->ne[1]);
-  for (int y = 0; y < p->ne[1]; y++) {
-      for (int x = 0; x < p->ne[0]; x++) {
-          printf("%.2f ", *(float *) ((char *) p->data + y * p->nb[1] + x * p->nb[0]));
-       }
-      printf("\n");
-  }
 
   ggml_free(ctx);
   return 0;
